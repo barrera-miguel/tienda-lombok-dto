c79e31bd377370be03ad2dbe9926b56344ab5bab
Miguel Barrera Oltra <miguelbo138@gmail.com>
2025-10-15 20:06:49 -0300

    Carga final

--- src/main/java/com/tienda/config/MongoDBConnection.java
+++ src/main/java/com/tienda/config/MongoDBConnection.java
@@ -8,7 +8,7 @@ public class MongoDBConnection {
     private static MongoClient mongoClient;
     private static MongoDatabase database;
 
-    // Configuración de conexión - ajustar según tu entorno
+    // Configuración de conexión
     private static final String CONNECTION_STRING = "mongodb://localhost:27017";
     private static final String DATABASE_NAME = "tienda_db";
 
